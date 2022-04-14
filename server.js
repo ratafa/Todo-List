2a34f9515e6aa02981cc80341363496d64c29d83
ratafa <aacz12035@gmail.com>
2022-04-14 23:25:19 +0900

    add searching

--- server.js
+++ server.js
@@ -11,10 +11,11 @@ app.use('/public', express.static('public'));
 const methodOverride = require('method-override');
 app.use(methodOverride('_method'));
 app.set('view engine', 'ejs');
+require('dotenv').config();
 
 
 var db;
-MongoClient.connect('mongodb+srv://aacz1203:asd88445522@cluster0.1aaxe.mongodb.net/TodoApp?retryWrites=true&w=majority', { useUnifiedTopology: true }, function (error, client) {
+MongoClient.connect(process.env.DB_URL, { useUnifiedTopology: true }, function (error, client) {
     if (error) return console.log(error)
     db = client.db('TodoApp');
 
@@ -45,24 +46,7 @@ app.get('/', function (req, res) {
 app.get('/write', function (req, res) {
     // res.sendFile(__dirname + '/write.ejs');
     res.render('write.ejs');
-});
-
-app.post('/add', function (req, res) {
-    db.collection('counter').findOne({ name: '게시물갯수' }, function (error, result) {
-        var postCounter = result.totalPost
-        // findeOne은 collection의 데이터 중 한 개의 데이터만 찾고 싶을 때 사용
-        // let이나 const보다 var을 사용한 이유는 var의 범위가 {}가 아닌 funtion의 범위이기 때문
-        
-        db.collection('POST').insertOne({ _id : (postCounter + 1), 제목: req.body.title, 날짜: req.body.date }, function (error, result) {
-            console.log('저장완료');
-            db.collection('counter').updateOne({name:'게시물갯수'}, { $inc : {totalPost:1} }, function(error, result) {
-                // $inc는 기본값에 더해줄 값을 설정 가능. 하지만 어떤 값을 변경할지 설정할 떄는 {totalPost}와 같은 오퍼레이터 형태로 기입 필요.
-                if(error) {return console.log(error)};
-                res.send('전송완료');
-            });
-        });
-    });
-});             
+});     
 
 app.get('/list', function (req, res) {
     db.collection('POST').find().toArray(function (error, result) {
@@ -73,14 +57,51 @@ app.get('/list', function (req, res) {
     });
 });
 
+
+app.get('/search', (req, res)=>{
+  var searchCondition =  [
+    {
+      $search: {
+        index: 'titleSearch',
+        text: {
+          query: req.query.value,
+          path: '제목'  // 제목날짜 둘다 찾고 싶으면 ['제목', '날짜']
+        }
+      }
+    }
+  ] 
+  console.log(req.query);
+  db.collection('POST').aggregate(searchCondition).toArray((에러, 결과)=>{
+    // 몽고db 아틀라스의 search 기능 이용
+    // https://codingapple.com/course-status/
+    console.log(결과)
+    res.render('search.ejs', {posts : 결과})
+  })
+})
+
+// app.get('/search', (req, res)=>{
+//   console.log(req.query);
+//   db.collection('POST').find( { $text : {$search: req.query.value}}).toArray((에러, 결과)=>{
+//     console.log(결과);
+//     res.render('search.ejs', { posts : 결과});
+//   });
+// })
+ 
+// app.get('/search', (req, res) => {
+//    console.log(req.query.value)
+//    db.collection('POST').find({제목:req.query.value}, function(error, result){
+//      console.log(result)
+//    });
+// });
+
 app.delete('/delete', function(req, res){
     console.log(req.body);
     req.body._id = parseInt(req.body._id);
     db.collection('POST').deleteOne(req.body, function(error, result){
         console.log('삭제완료');
         res.status(200).send({message : '성공했습니다'});
-        // 실제로 요청이 성공했는지를 알려줘야지 list.ejs에서 요청 반응을 함
-        // 그렇기에 ejs랑 호환을 하려면 서버쪽에서 요청 성공이 했는지 알려주는 것이 중요함
+        // 실제로 요청이 성공했는지를 알려줘야지 list.ejs에서 req 반응을 함
+        // 그렇기에 ejs랑 호환을 하려면 서버쪽에서 req 성공이 했는지 알려주는 것이 중요함
     });
 });
 
@@ -91,7 +112,7 @@ app.get('/detail/:id', function(req, res){
         console.log('result');
         res.render('detail.ejs', { data : result });
         // res.status(400).send('페이지를 찾을 수 없습니다.');
-        // 없는 _id로 파라미터 요청 시, 에러 페이지를 띄워줌.
+        // 없는 _id로 파라미터 req 시, 에러 페이지를 띄워줌.
     });
 }); 
 
@@ -102,3 +123,95 @@ app.get('/edit/:id', function (req, res) {
         res.render('edit.ejs', { post : result });
     });
 });
+
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
+
+app.use(session({secret : '0826', resave : true, saveUninitialized: false}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/login', function(req, res){
+    res.render('login.ejs');
+});
+
+app.post('/login', passport.authenticate('local', {
+    failureRedirect : '/fail'
+}), function(req, res){
+    res.redirect('/')
+});
+
+
+app.get('/mypage', checkLogin, function(req, res){
+  console.log(req.user);
+  res.render('mypage.ejs', {loginUser : req.user});
+});
+
+function checkLogin(req, res, next){
+  if (req.user){
+    next()
+  } else{
+    res.send('로그인을 해주세요')
+  }};
+  // checkLogin과 같이 mypage를 get 하는 중간에 
+  // 로그인 여부 확인 기능을 사이에 넣을 수 있는데 이를 미들웨어라고 한다.
+  
+  
+  passport.use(new LocalStrategy({
+    usernameField: 'id',
+    passwordField: 'pw',
+    session: true,
+    passReqToCallback: false,
+  }, function (inputId, inputPw, done) {
+    console.log(inputId, inputPw);
+    db.collection('login').findOne({ id: inputId }, function (error, result) {
+      if (error) return done(error)
+    // = 데이터 베이스에 에러가 뜨면 에러 처리를 해주세요
+      if (!result) return done(null, false, { message: '존재하지않는 아이디요' })
+      //  = 결과에 아무것도 안 담겨있을 때 message를 실행시켜주세요
+      if (inputPw == result.pw) {
+        return done(null, result)
+      } else {
+        return done(null, false, { message: '비번틀렸어요' })
+      }  
+    })
+  }));
+  
+  passport.serializeUser(function (user, done) {
+    done(null, user.id)
+  });
+  //   -> 세션을 저장시키는 코드(로그인 성공 시 실행)
+  
+  passport.deserializeUser(function (id, done) {
+    db.collection('login').findOne({id : id}, function(error, result){
+      done(null, result)
+    })
+  }); 
+  //   -> 이 세션 데이터 가진 사람을 DB에서 찾아주세요(마이페이지 접속시 실행)
+  
+  app.post('/add', function (req, res) {
+    db.collection('counter').findOne({ name: '게시물갯수' }, function (error, result) {
+        var postCounter = result.totalPost
+        // findeOne은 collection의 데이터 중 한 개의 데이터만 찾고 싶을 때 사용
+        // let이나 const보다 var을 사용한 이유는 var의 범위가 {}가 아닌 funtion의 범위이기 때문
+        var saveInfo = { _id : (postCounter + 1), 제목: req.body.title, 날짜: req.body.date, 작성자: req.user._id};
+        
+        db.collection('POST').insertOne(saveInfo, function (error, result) {
+            console.log('저장완료');
+            db.collection('counter').updateOne({name:'게시물갯수'}, { $inc : {totalPost:1} }, function(error, result) {
+                // $inc는 기본값에 더해줄 값을 설정 가능. 하지만 어떤 값을 변경할지 설정할 떄는 {totalPost}와 같은 오퍼레이터 형태로 기입 필요.
+                if(error) {return console.log(error)};
+                res.send('전송완료');
+            });
+        });
+    });
+});        
+// 회원기능이 필요하면 passport 셋팅하는 부분이 회원가입 코드보다 위에 있어야 함
+
+  app.post('/register', function (req,res) {
+    db.collection('login').insertOne({ id: req.body.id, pw: req.body.pw }, function (error, result) {
+      res.redirect('/');
+    })
+  })
